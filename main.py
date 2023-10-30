41df823179e169eb20ce5494ac387cf1c08a2f94
Fritsch <filip.fritsch@gmail.com>
2023-10-30 18:47:46 +0100

    fixed wrong merge

--- main.py
+++ main.py
@@ -16,13 +16,8 @@ tree = app_commands.CommandTree(client)
 @client.event
 async def on_ready():
     await tree.sync(guild=discord.Object(id=guild_id))
-
-tree = app_commands.CommandTree(client)
-
-@client.event
-async def on_ready():
-    await tree.sync(guild=discord.Object(id=guildID))
     print("Ready!")
+
 
 @client.event
 async def on_message(message):
@@ -31,7 +26,8 @@ async def on_message(message):
     
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
-        
+
+
 @tree.command(name = "join_channel", description = "Makes the Dodi bot join your voice channel.", guild=discord.Object(id=guild_id))
 async def join_channel(interaction):
     channel = interaction.channel
@@ -46,6 +42,7 @@ async def join_channel(interaction):
         voice_channel = interaction.user.voice.channel
         await voice_channel.connect()
 
+
 @tree.command(name = "leave_channel", description = "Makes the Dodi bot leave your voice channel.", guild=discord.Object(id=guild_id))
 async def leave_channel(interaction):
     channel = interaction.channel
@@ -56,6 +53,7 @@ async def leave_channel(interaction):
         await channel.send(f"👋 Farewell, {interaction.user}! It's been a blast, but I must go for now. 👋\n🎤 Mic drop! Leaving the stage... I mean, the voice channel. 🎤\n🏃‍♂️ Zoom! I'm outta here. Thanks for the chitchat and tunes! 🏃‍♂️")
         for vc in interaction.client.voice_clients:
             await vc.disconnect()
+
 
 @tree.command(name="upload_file",
               description="Here you can upload a txt file to the bot",
