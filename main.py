ae9cf2519ea5eb446e545803594e3ea4e6251b91
Antonia <still.antonia@gmx.de>
2023-10-30 14:15:48 +0100

    first commit :)

---
+++ main.py
@@ -0,0 +1,21 @@
+import discord
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+token = os.getenv('DISCORD_BOT_SECRET')
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run(token)
